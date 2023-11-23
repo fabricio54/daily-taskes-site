14525c5793b1f2c4443a9aa3fbb9aeb9312c5a2b
Fabricio Alves <fabricioallves2020@gmail.com>
2023-11-23 14:37:41 -0300

    atualização do site

--- src/database/conect.js
+++ src/database/conect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connectDatabase() {
   console.log("Aguardando conexão com o banco de dados...");
   try {
-    await mongoose.connect("mongodb+srv://fabricio:Kpnu8UWH0VorAJcd@cluster0.xv0badm.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect("mongodb+srv://fabricio:fabricio@cluster0.xv0badm.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("Mongo Atlas Connected");
   } catch (error) {
     console.log(error);
